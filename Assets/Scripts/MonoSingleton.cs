b32dfdcc4064d1e90a959350ebc59fd382d681d6
morlando88 <57946983+morlando88@users.noreply.github.com>
2019-11-25 23:09:15 +0100

    Created Spawn manager
    Spawn manager spawns two shapes outside frame to be used at a later time.

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance = null;
+    public static T Instance // Captical because this is a property 
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>(); // Create a new game object and add component type of T
+                instance.Init();
+            }
+
+            return instance;
+
+        }
+    }
+
+    public virtual void Init() { }
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this as T; 
+    }
+   
+}
---
+++ Assets/Scripts/SpawnManagerBus.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// refrence to all spawn points in level and list of all object we can spawn 
+
+public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
+{
+    public GameObject Square;
+    // declare and initialize lists
+    public List<Transform> spawnPoint = new List<Transform>();
+    public List<GameObject> spawnPrefabs = new List<GameObject>();
+
+
+
+    public void Spawn(int spawnPrefabIndex)
+    {
+        Spawn(spawnPrefabIndex, 0);
+    }
+    public void Spawn(int spawnPrefabIndex, int spawnPointIndex)
+    {
+        Instantiate(spawnPrefabs[spawnPrefabIndex], spawnPoint[spawnPointIndex].position, spawnPoint[spawnPointIndex].rotation);
+    }
+
+ /*   
+    public static void Main()
+    {
+        public int ObjectNum = spawnPrefabs.Count();
+    }
+*/    
+
+    void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            Spawn(i);
+        }
+    }
+}
+
+ /*   
+    //Temp
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Spawn(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            Spawn(1);
+
+    }
+}
+*/
+
+
+  
