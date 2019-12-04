6e7c52ea5d3e0a6f2999f109786e007d782c0605
morlando88 <57946983+morlando88@users.noreply.github.com>
2019-12-04 18:39:38 +0100

    Added End Screen and changed bus stops to spawn

--- Assets/Scripts/SpawnManagerBus.cs
+++ Assets/Scripts/SpawnManagerBus.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // refrence to all spawn points in level and list of all object we can spawn 
 
@@ -9,16 +10,28 @@ public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
 {
     public GameObject TempGameObject;
     public GameObject bus;
+    public GameObject scenery;
 
     // declare and initialize lists
-    public List<Transform> spawnPoint = new List<Transform>();
 
     public List<GameObject> spawnPrefabs = new List<GameObject>();
     public List<GameObject> spawned = new List<GameObject>();
     public int SpawnsPerStop = 4;
     public int UnspawnsPerStop = 2;
+    public bool _egg = false;
+    public bool _star = false;
+    public bool _orange = false;
 
+    private GameObject _EndGameMenu;
 
+    void Start()
+    {
+        _EndGameMenu = GameObject.Find("EndGameMenu");
+        _EndGameMenu.SetActive(false);
+        bus.GetComponent<BusMovement>().manager = this;
+        
+
+    }
 
 
 
@@ -30,7 +43,9 @@ public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
 
         if (spawnPrefabs.Count > 0)
         {
-            TempGameObject = Instantiate(spawnPrefabs[spawnPrefabIndex], spawnPoint[(spawnPointIndex)].position, spawnPoint[spawnPointIndex].rotation);
+            Vector3 spawnposition = new Vector3(bus.transform.position.x + 10, bus.transform.position.y, 0);
+            TempGameObject = Instantiate(spawnPrefabs[spawnPrefabIndex], spawnposition, Quaternion.identity);
+            Debug.Log(TempGameObject.name);
             TempGameObject.GetComponent<CharacterMovement>().bus = bus;
             TempGameObject.GetComponent<CharacterMovement>().firstspawned = true;
             spawned.Add(TempGameObject);
@@ -55,7 +70,7 @@ public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
             g.transform.position = new Vector3(bus.transform.position.x, g.transform.position.y, 0) ;
             //Instantiate(g, bus.transform.position, Quaternion.identity);
             spawned.Remove(g);
-            StartCoroutine(RemovePlayer(g));
+            //StartCoroutine(RemovePlayer(g));
             }
 
         unspawnnum++;
@@ -91,18 +106,14 @@ public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
 
     }
 
+    /*
     private IEnumerator RemovePlayer(GameObject g)
     {
         yield return new WaitForSeconds(3f);
         GameObject.DestroyImmediate(g);
     }
+    */
 
-
-
-    void Start()
-    {
-        bus.GetComponent<BusMovement>().manager = this;
-    }
 
 
     private bool objSpawned;
@@ -156,7 +167,49 @@ public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
         yield return new WaitForSeconds(5f);
         bus.GetComponent<BusMovement>().go = true;
     }
-    
+
+    public void endgame()
+    {
+        _EndGameMenu.SetActive(true);
+        scenery.SetActive(false);
+        bus.SetActive(false);
+    }
+
+    public void checkResults()
+    {
+        for(int i = 0; i < spawned.Count; i++)
+        {
+            if (spawned[i].name == "Character_Egg(Clone)")
+                _egg = !_egg;
+            if (spawned[i].name == "Character_Star(Clone)")
+                _star = !_star;
+            if (spawned[i].name == "Character_Orange(Clone)")
+                _orange = !_orange;
+        }
+        if (_egg || _orange || _star)
+        {
+            Debug.Log("you suck");
+        }
+        else
+            Debug.Log("Yay");
+    }
+    #region ToggleFUNctions
+    public void eggpress()
+    {
+        _egg = !_egg;
+    }
+
+    public void starpress()
+    {
+        _star = !_star;
+    }
+
+    public void orangepress()
+    {
+        _orange = !_orange;
+    }
+
+    #endregion
 }
 
 
@@ -166,4 +219,3 @@ public class SpawnManagerBus : MonoSingleton<SpawnManagerBus>
 
 
 
-  
